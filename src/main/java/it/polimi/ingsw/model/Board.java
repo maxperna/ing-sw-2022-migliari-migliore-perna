a2926a4738ffa25f95429bbead14f21d5f700480
AleM24 <alessio.migliore@mail.polimi.it>
2022-03-24 14:42:47 +0100

    Created class Board

--- src/main/java/it/polimi/ingsw/model/Board.java
+++ src/main/java/it/polimi/ingsw/model/Board.java
@@ -1,4 +1,60 @@
 package it.polimi.ingsw.model;
 
-public class Board {
+import it.polimi.ingsw.*;
+import it.polimi.ingsw.Player;
+
+
+import java.util.ArrayList;
+import java.util.Map;
+
+
+public class Board implements StudentManager {
+
+    private int numberOfPlayer;
+    private int boardID;
+    private Player player;
+    private ArrayList<Student> studentsOutside;
+    private Map<Color, Teacher> teachers;
+    private ArrayList<Tower> towers;
+
+    public int getBoardID(){
+        return this.boardID;
+    }
+
+    public void getTeachers(){
+
+    }
+
+    public void addTeachers(){
+
+    }
+
+    public void removeTeacher(){
+
+    }
+
+    public void moveInside(){
+
+    }
+
+    public void moveToIsland(){
+
+    }
+
+    public void moveTower(){
+
+    }
+
+    public void addTower(){
+
+    }
+
+    public Board(){
+
+    }
+
+    public int colorStudent(Color color){
+        return 0;
+    }
+
 }
