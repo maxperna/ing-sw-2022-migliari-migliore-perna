b84d623325a67c8bb8d74984744d84197bdf47c7
AleM24 <alessio.migliore@mail.polimi.it>
2022-03-24 14:28:50 +0100

    Created class CardDeck

--- src/main/java/it/polimi/ingsw/model/CardDeck.java
+++ src/main/java/it/polimi/ingsw/model/CardDeck.java
@@ -1,4 +1,32 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.model.*;
+import it.polimi.ingsw.model.factory.Card;
+
+import java.util.ArrayList;
+
 public class CardDeck {
+
+    private DeckType deckID;
+    private ArrayList<Assistants> cards;
+    private Card lastCardUsed;
+
+    public ArrayList<Assistants> getRemainingCards(){
+        return cards;
+    }
+
+    public DeckType getDeckType(){
+        return this.deckID;
+    }
+
+    public Card getLastCard(){
+        return lastCardUsed;
+    }
+
+    public void removeCard(){
+
+    }
+
+    public CardDeck(){}
+
 }
