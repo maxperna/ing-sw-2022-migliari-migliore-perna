9b2e4a50cafbf942575c85971993adfac4edf127
AleM24 <alessio.migliore@mail.polimi.it>
2022-03-24 11:42:41 +0100

    Created classes IslandTile, Board, GameField, Player

--- src/main/java/it/polimi/ingsw/Player.java
+++ src/main/java/it/polimi/ingsw/Player.java
@@ -1,4 +1,37 @@
 package it.polimi.ingsw;
 
+
+import it.polimi.ingsw.components.Board;
+import it.polimi.ingsw.components.CardDeck;
+
 public class Player {
+
+    private final String nickname;
+    private Board board;
+    private CardDeck deck;
+
+    public String getNickname(){
+        return this.nickname;
+    }
+
+
+    public void setBoard(){
+
+    }
+
+
+    public Board getBoard(){
+        return this.board;
+    }
+
+
+    public void setDeck()
+    {
+
+    }
+
+
+    public Player(String nickname){
+        this.nickname=nickname;
+    }
 }
--- src/main/java/it/polimi/ingsw/components/GameField.java
+++ src/main/java/it/polimi/ingsw/components/GameField.java
@@ -1,4 +1,29 @@
 package it.polimi.ingsw.components;
 
-public class GameField {
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class GameField{
+
+    private LinkedList<IslandTile> islands;
+    private int numberOfIslands;
+    private Pouch pouch;
+    private ArrayList<CloudTile> cloudsTile;
+
+    public void mergeIsland(){
+
+    }
+
+    public void rechargeCloud(){
+
+    }
+
+    public void moveMotherNature(){
+
+    }
+
+    private GameField(){
+
+    }
 }
