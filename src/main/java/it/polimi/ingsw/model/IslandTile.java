34e7ae56bf1efdd53099dc61c64777512c3e3edc
AleM24 <alessio.migliore@mail.polimi.it>
2022-03-24 14:20:05 +0100

    Created class IslandTile

--- src/main/java/it/polimi/ingsw/model/IslandTile.java
+++ src/main/java/it/polimi/ingsw/model/IslandTile.java
@@ -1,4 +1,64 @@
 package it.polimi.ingsw.model;
 
-public class IslandTile {
+import it.polimi.ingsw.*;
+
+import java.util.ArrayList;
+
+
+/**
+ * Class IslandTile
+ * @author Alessio Migliore
+ */
+public class IslandTile implements StudentManager {
+    private static int tileID;
+    private ArrayList<Student> students;
+    private MotherNature motherNature;
+    private Tower tower;
+    private String mostInfluencePlayer;
+
+
+    /**
+     * Method getID
+     * @return island identifier
+     */
+    public int getID() {
+        return this.tileID;
+    }
+
+    /**
+     * Method setTower, updates the tower attribute after tower construction or substitution
+     *
+     */
+    public void setTower(){
+    }
+
+    /**
+     * Method getTowerColor
+     * @return color of placed tower
+     */
+    public Tower getTowerColor(){
+        return this.tower/*.getColor()*/;
+    }
+
+    /**
+     * Method setMostInfluence, checks the color of the highest number of students placed on the island
+     *
+     */
+    public void setMostInfluencePlayer(){
+
+    }
+
+    public String getMostInfluencePlayer(){
+        return this.mostInfluencePlayer;
+    }
+
+    public IslandTile(int tileID){
+        this.tileID=tileID;
+    }
+
+    public int colorStudent(Color color){
+        return 0;
+    };
+
+
 }
