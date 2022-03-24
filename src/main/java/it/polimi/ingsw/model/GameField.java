6194127231c7b06dceab366befcc8205d6895820
AleM24 <alessio.migliore@mail.polimi.it>
2022-03-24 14:39:56 +0100

    Created class GameField

--- src/main/java/it/polimi/ingsw/model/GameField.java
+++ src/main/java/it/polimi/ingsw/model/GameField.java
@@ -1,4 +1,29 @@
 package it.polimi.ingsw.model;
 
-public class GameField {
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class GameField{
+
+    private LinkedList<IslandTile> islands;
+    private int numberOfIslands;
+    private Pouch pouch;
+    private ArrayList<CloudTile> cloudsTile;
+
+    public void mergeIsland(){
+
+    }
+
+    public void rechargeCloud(){
+
+    }
+
+    public void moveMotherNature(){
+
+    }
+
+    public GameField(){
+
+    }
 }
