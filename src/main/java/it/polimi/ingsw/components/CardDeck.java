3596df3fbb1f4602e00be95ed29371967ce13d7a
AleM24 <alessio.migliore@mail.polimi.it>
2022-03-24 13:47:34 +0100

    Created class IslandTile

--- src/main/java/it/polimi/ingsw/components/CardDeck.java
+++ src/main/java/it/polimi/ingsw/components/CardDeck.java
@@ -1,4 +1,32 @@
 package it.polimi.ingsw.components;
 
+import it.polimi.ingsw.Assistants;
+import it.polimi.ingsw.DeckType;
+
+import java.util.ArrayList;
+
 public class CardDeck {
+
+    private DeckType deckID;
+    private ArrayList<Assistants> cards;
+    private Card lastCardUsed;
+
+    public ArrayList<Assistants> getRemainingCards(){
+        return cards;
+    }
+
+    public DeckType getDeckType(){
+        return this.deckID;
+    }
+
+    public Card getLastCard(){
+        return lastCardUsed;
+    }
+
+    public void removeCard(){
+
+    }
+
+    public CardDeck(){}
+
 }
