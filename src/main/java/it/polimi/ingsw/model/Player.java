956c471b5dc257d2e706280c582077cc5fd991ea
AleM24 <alessio.migliore@mail.polimi.it>
2022-03-24 14:33:03 +0100

    Created class Player

--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -1,4 +1,35 @@
 package it.polimi.ingsw.model;
 
+
+import it.polimi.ingsw.model.*;
+
 public class Player {
+
+    private final String nickname= "...";
+    private Board board;
+    private CardDeck deck;
+
+    public String getNickname(){
+        return this.nickname;
+    }
+
+
+    public void setBoard(){
+
+    }
+
+
+    public Board getBoard(){
+        return this.board;
+    }
+
+
+    public void setDeck()
+    {
+
+    }
+
+
+    public Player(){
+    }
 }
