051a7cdd94876a5d94e83d7be39674413422f661
Lorenzo Migliari <miglia.migliari@gmail.com>
2022-03-27 03:18:20 +0200

    Ulteriore pulizia codice dopo FixFactory

--- src/main/java/it/polimi/ingsw/model/factory/FourPlayersFIeld.java
+++ src/main/java/it/polimi/ingsw/model/factory/FourPlayersFIeld.java
@@ -3,7 +3,7 @@ package it.polimi.ingsw.model.factory;
 public class FourPlayersFIeld implements GameFieldCreator{
     @Override
     public void newField() {
-        System.out.println("Created 4 players Field");
+        System.out.println("Created 4 players Field\n");
     }
 
 }
--- src/main/java/it/polimi/ingsw/model/factory/ThreePlayersField.java
+++ src/main/java/it/polimi/ingsw/model/factory/ThreePlayersField.java
@@ -4,6 +4,6 @@ public class ThreePlayersField implements GameFieldCreator {
 
     @Override
     public void newField() {
-        System.out.println("Created 3 players Field");
+        System.out.println("Created 3 players Field\n");
     }
 }
