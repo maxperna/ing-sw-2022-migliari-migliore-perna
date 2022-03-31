f1aa4723f2433f297e302ee016eac0fa6433c63c
maxperna <maxperna@users.noreply.github.com>
2022-03-31 16:31:23 +0200

    Correzione board secondo nuova implementazione di Student, inserimento prime eccezioni

--- src/main/java/it/polimi/ingsw/model/Board.java
+++ src/main/java/it/polimi/ingsw/model/Board.java
@@ -1,68 +1,87 @@
 package it.polimi.ingsw.model;
 
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 
 public class Board implements StudentManager {
 
     private final int hallDimension;       //maximum number of players in the external hall depending by the num of players
-    private final int numOfTower;        //maximum number of towers depending by the number of players
+    private final int maxNumOfTowers;        //maximum number of towers depending by the number of players
+    private final Color towerColor;      //color of the towe on the board
     private final UUID boardID;
-    private ArrayList<Student> studentsOutside;
-    private Map<Color, Teacher> teachers;
-    private ArrayList<Tower> towers;
+    private ArrayList<Color> studentsOutside;    //list of student in the outer room
+    private Map<Color,Integer> lectureHall;      //list of student for each color inside the main hall
+    private Map<Color, Boolean> teachers;       //map to signal the presence of a teacher on the board
+    private int numOfTowers;  //number of towers on the board
 
     //Private constructor used by the factory
-    private Board(int hallDimension,int numOfTower){
-        this.hallDimension = hallDimension;
-        this.numOfTower = numOfTower;
+    private Board(int hallDimension,int numOfTower,Color towerColor){       //num of towers is a parameter which depends on the num of players
+        this.hallDimension = hallDimension;           //hall dimension depends on the num of players
+        this.maxNumOfTowers = numOfTower;
+        this.towerColor = towerColor;
         this.boardID = UUID.randomUUID();         //thread safe auto generated ID
+        this.lectureHall = new HashMap<Color,Integer>();
+
     }
 
     //Method used by the factory to create a new board
-    public static Board createBoard(int hallDimension,int numOfTower){
-        return new Board(hallDimension,numOfTower);
+    public static Board createBoard(int hallDimension,int numOfTower,Color towerColor){
+        return new Board(hallDimension,numOfTower,towerColor);
     }
 
     public UUID getBoardID(){
         return this.boardID;
     }
 
-    public ArrayList<Student> getStudentsOutside(){
+    public ArrayList<Color> getStudentsOutside(){
         return studentsOutside;
     }
-
-    public Map<Color, Teacher> getTeachers() {
-        return teachers;
-    }
-
-    public ArrayList<Tower> getTowers() {
-        return towers;
+    //da modificare
+    public int getNumOfTowers() {
+        return numOfTowers;
     }
 
     //Method to get the presence of a teacher on the board
-    public boolean getTeachers(Color color){return true;}
+    public boolean getTeacher(Color color){
+        return teachers.get(color);
+    }
     //Method to add the teacher on the board
-    public void addTeachers(Teacher teacher){}
+    public void addTeachers(Color color){
+        teachers.put(color,true);
+    }
     //Method to remove the teacher on the board
-    public void removeTeacher(Teacher teacher){}
+    public void removeTeacher(Color color){
+        teachers.put(color, false);
+    }
     //Method to move the student from the external hall to the internal one
-    public void moveInside(Student student){}
-    //Method to move the student from the board to the island
-    public void moveToIsland(Student student, IslandTile targetIsland){}
-    //Method to move a tower on an Island
-    public void moveTower(){}
-    //Method to add the tower on the board
-    public void addTower(){}
+    public void moveInside(Color color) throws NotOnBoardException{     //Not on board da implementare
+        if(!studentsOutside.contains(color)) throw NotOnBoardException;
+        else{
 
-    public void setStudentsOutside(ArrayList<Student> studentsOutside) {
+        }
+    }
+    //Method to move the student from the board to the island
+    public void moveToIsland(Color color, IslandTile targetIsland) throws NotOnBoardException{
+        if(!studentsOutside.contains(color)) throw NotOnBoardException;
+    }
+    //Method to move a tower on an Island
+    public void moveTower() throws EndGameException{
+        if(numOfTowers==0) throw EndGameException;
+        else{
+            numOfTowers--;
+        }
+    }              //throw an exception which signals the end of the game
+    //Method to add the tower on the board
+    public void addTower(){
+        numOfTowers++;
+    }
+
+    public void setStudentsOutside(ArrayList<Color> studentsOutside) {
         this.studentsOutside = studentsOutside;
     }
 
-    public void setTowers(ArrayList<Tower> towers) {
-        this.towers = towers;
+    public void setTowers(int maxNumOfTowers) {
+        this.towers = maxNumOfTowers;
     }
 
     public int colorStudent(Color color){
