b92dc78a1fa06110bcd50b749c38492fe5c0636f
AleM24 <alessio.migliore@mail.polimi.it>
2022-04-01 17:42:03 +0200

    Completed Pouch and CloudTile, added their respective Exceptions

--- src/main/java/it/polimi/ingsw/model/CloudTile.java
+++ src/main/java/it/polimi/ingsw/model/CloudTile.java
@@ -1,29 +1,40 @@
 package it.polimi.ingsw.model;
+import it.polimi.ingsw.exceptions.EmptyCloudException;
+
 import java.util.*;
 
 /**
  * Class Cloud Tile, one for each player, contains MAX 3 students
- * @author Miglia
+ * @author Alessio Migliore
  */
 public class CloudTile {
 
     private int tileID;
-    private ArrayList<Student> students;
+    private ArrayList<Color> students;
 
     /**
-     * @return a string with the number and color of students on the tile, returns "empty" if there are none
+     * @return an Arraylist of Color that represents students on the cloud
+     * @throws EmptyCloudException when there are no students on the cloud
      */
-    public String getStudent() {
-        String studentsString = "Empty";
-
-        return studentsString;
+    public ArrayList<Color> getStudent() throws EmptyCloudException{
+        if(students.isEmpty())
+            throw new EmptyCloudException();
+        else
+            return students;
     }
 
     /**
-     * @return an ArrayList of three students on the cloud and deletes them from ArrayList of the cloud, returns null if there are none
+     * @throws EmptyCloudException when there are no students on the cloud
+     * @return an ArrayList of three students on the cloud and deletes them from ArrayList of the cloud
      */
-    public ArrayList<Student> moveStudents() {
-
-        return new ArrayList<Student>();
+    public synchronized ArrayList<Color> moveStudents() throws EmptyCloudException{
+        if(students.isEmpty())
+            throw new EmptyCloudException();
+        else {
+            ArrayList<Color> studentsReturned = new ArrayList<Color>();                                                 //creates an ArrayList in which all students are copied
+            studentsReturned.addAll(students);
+            students.removeAll(students);                                                                               //empties the ArrayList students
+            return studentsReturned;                                                                                    //returns an ArrayList containing a copy of the previous students in ArrayList students
+        }
     }
 }
