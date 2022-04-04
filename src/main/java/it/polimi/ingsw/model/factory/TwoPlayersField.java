7704da4b0d822b1ab8b9c1b42f60712b5a0cbcd3
Lorenzo Migliari <miglia.migliari@gmail.com>
2022-04-04 15:03:48 +0200

    Implementing new LinkedList

--- src/main/java/it/polimi/ingsw/model/factory/TwoPlayersField.java
+++ src/main/java/it/polimi/ingsw/model/factory/TwoPlayersField.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.model.factory;
 
+import it.polimi.ingsw.CircularLinkedList.IslandList;
 import it.polimi.ingsw.exceptions.NotEnoughElements;
 import it.polimi.ingsw.model.Color;
 import it.polimi.ingsw.model.Game;
@@ -26,10 +27,10 @@ public class TwoPlayersField implements GameFieldCreator{
 
         int noStudentTile = (int) Math.floor(Math.random()*(6)+1);
 
-        ArrayList<ArrayList<IslandTile>> allIslands = new ArrayList<>();
+        IslandList islandList = new IslandList();
+        ArrayList<IslandTile>  islandTiles = new ArrayList<>();
         for(int i = 1; i <= Game.maxTile; i++)
         {
-            ArrayList<IslandTile>  islandTile = new ArrayList<>();
             IslandTile tile = new IslandTile(i);
 
             if((i != noStudentTile) & (i != noStudentTile*2)) {
@@ -40,20 +41,10 @@ public class TwoPlayersField implements GameFieldCreator{
                 }
             }
 
-            islandTile.add(tile);
-            allIslands.add(islandTile);
-
+            islandTiles.add(tile);
         }
+        islandList.addIslands(islandTiles);
 
-
-            for ( int j = 0; j < allIslands.size(); j++)
-            {
-                for (int k = 0; k < allIslands.get(j).size(); k++) {
-                    System.out.println(allIslands.get(j).get(k).getID());
-                    if((j+1 != noStudentTile) & (j+1 != noStudentTile*2))
-                        System.out.println(allIslands.get(j).get(k).getStudents().get(0) + "\n");
-                }
-            }
-
+        System.out.println("ciao");
     }
 }
