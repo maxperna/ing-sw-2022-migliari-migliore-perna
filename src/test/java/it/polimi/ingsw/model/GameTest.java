ed072fb6165a01de98716839495149e0d4aab6a7
Lorenzo Migliari <miglia.migliari@gmail.com>
2022-04-15 15:05:06 +0200

    GameTest

---
+++ src/test/java/it/polimi/ingsw/model/GameTest.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void drawFromPool() {
+    }
+
+    @Test
+    void getGameID() {
+    }
+
+    @Test
+    void getGameField() {
+    }
+
+    @Test
+    void getPlayersList() {
+    }
+}
