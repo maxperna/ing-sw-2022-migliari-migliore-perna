1fb7549e6d74e926afec985f9154337ce149ad51
maxperna <maxperna@users.noreply.github.com>
2022-05-02 18:06:01 +0200

    Caricamento assistenti

---
+++ src/main/java/it/polimi/ingsw/model/experts/Expert11.java
@@ -0,0 +1,66 @@
+package it.polimi.ingsw.model.experts;
+
+import it.polimi.ingsw.exceptions.IllegalMove;
+import it.polimi.ingsw.exceptions.NotEnoughCoin;
+import it.polimi.ingsw.exceptions.NotEnoughSpace;
+import it.polimi.ingsw.exceptions.NotEnoughStudentsException;
+import it.polimi.ingsw.model.Color;
+import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.Player;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Expert11 implements ExpertCard {
+
+    private int cost = 2;
+    private final Game currentGame;
+    private final ArrayList<Color> studentsOnCard = new ArrayList<>();
+
+    public Expert11(Game currentGame){
+        this.currentGame = currentGame;
+
+        try {
+            this.studentsOnCard.addAll(currentGame.getPouch().randomDraw(4));
+        }
+        catch(NotEnoughStudentsException e){
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void useCard(Player user,Color colorToAdd) throws NotEnoughCoin, IllegalMove {
+        if(user.getNumOfCoin()<this.cost)
+            throw new NotEnoughCoin();
+        else{
+            user.addCoin(-cost);
+            this.cost++;
+
+            if (!studentsOnCard.contains(colorToAdd)) throw new IllegalMove("Student is not on card");
+            else {
+                try {
+                    studentsOnCard.remove(colorToAdd);
+                    user.getBoard().addStudentsDiningRoom(new ArrayList<>(Collections.singleton(colorToAdd)));
+                    studentsOnCard.addAll(currentGame.getPouch().randomDraw(1));
+                }
+                catch(NotEnoughSpace | NotEnoughStudentsException e){
+                e.printStackTrace();
+                }
+        }
+
+        }
+    }
+
+    @Override
+    public void endEffect() {
+
+    }
+
+    @Override
+    public int getCost() {
+        return this.cost;
+    }
+
+    public ArrayList<Color> getStudentsOnCard(){
+        return this.studentsOnCard;
+    }
+}
