e3e330fc03f9922850ae1040431d85545b0e6a4b
maxperna <maxperna@users.noreply.github.com>
2022-05-02 18:06:16 +0200

    Caricamento assistenti

---
+++ src/main/java/it/polimi/ingsw/model/experts/Expert12.java
@@ -0,0 +1,56 @@
+package it.polimi.ingsw.model.experts;
+
+import it.polimi.ingsw.exceptions.NotEnoughCoin;
+import it.polimi.ingsw.exceptions.NotOnBoardException;
+import it.polimi.ingsw.model.Color;
+import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.Player;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Expert12 implements ExpertCard {
+    private int cost = 3;
+    private final Game currentGame;
+
+    public Expert12(Game currentGame){
+        this.currentGame = currentGame;
+    }
+
+    @Override
+    public void useCard(Player user, Color colorToRemove) throws NotEnoughCoin {
+        if(user.getNumOfCoin()<cost) throw new NotEnoughCoin();
+
+        else{
+            user.addCoin(-this.cost);
+            this.cost++;
+            //Temporary list
+            ArrayList<Color> colorToList = new ArrayList<>(Collections.singleton(colorToRemove));        //transform color to list as parameter adjustment
+            ArrayList<Color> studentToReinsert = new ArrayList<>();  //student to re-add to pouch
+
+            for(Player player:currentGame.getPlayersList()){
+                for(int i=0;i<3;i++){
+                    try {
+                        studentToReinsert.addAll(player.getBoard().moveFromDiningRoom(colorToList));
+                    }
+                    catch (NotOnBoardException e){
+                        e.printStackTrace();        //exit from the loop(player has less than 3 students of that color
+                        break;
+                    }
+                }
+            }
+            currentGame.getPouch().addStudents(studentToReinsert);
+
+        }
+    }
+
+    @Override
+    public void endEffect() {
+
+    }
+
+    @Override
+    public int getCost() {
+        return 0;
+    }
+}
