85e253bc3feb2d7e431d20e3ee00cf63bb15dffb
Lorenzo Migliari <lorenzo1.migliari@mail.polimi.it>
2022-05-12 08:30:44 +0200

    Update Message

--- src/main/java/it/polimi/ingsw/network/messages/FirstLoginMessage.java
+++ src/main/java/it/polimi/ingsw/network/messages/FirstLoginMessage.java
@@ -1,17 +1,26 @@
 package it.polimi.ingsw.network.messages;
 
+import it.polimi.ingsw.model.DeckType;
+import it.polimi.ingsw.model.TowerColor;
+
 import java.util.UUID;
 
 public class FirstLoginMessage extends Message {
 
+    private final TowerColor chosenTowerColor;
     private final String gameMode;
     private final boolean expertMode;
+    private final DeckType chosenDeckType;
     UUID identifier = UUID.randomUUID();
 
-    public FirstLoginMessage(String senderPlayer, String gameMode, boolean expertMode) {
+
+
+    public FirstLoginMessage(String senderPlayer, String gameMode, boolean expertMode, TowerColor chosenTowerColor, DeckType chosenDeckType) {
         super(MessageType.FIRST_LOGIN, senderPlayer);
         this.gameMode = gameMode;
         this.expertMode = expertMode;
+        this.chosenTowerColor = chosenTowerColor;
+        this.chosenDeckType = chosenDeckType;
     }
 
     public String getGameMode() {
@@ -20,5 +29,13 @@ public class FirstLoginMessage extends Message {
 
     public boolean isExpertMode() {
         return expertMode;
+    }
+
+    public TowerColor getChosenTowerColor() {
+        return chosenTowerColor;
+    }
+
+    public DeckType getChosenDeckType() {
+        return chosenDeckType;
     }
 }
--- src/main/java/it/polimi/ingsw/network/messages/LogInMessage.java
+++ src/main/java/it/polimi/ingsw/network/messages/LogInMessage.java
@@ -1,13 +1,27 @@
 package it.polimi.ingsw.network.messages;
 
+import it.polimi.ingsw.model.DeckType;
+import it.polimi.ingsw.model.TowerColor;
+
 import java.util.UUID;
 
 public class LogInMessage extends  Message{
 
     UUID identifier = UUID.randomUUID();
+    private final TowerColor chosenTowerColor;
+    private final DeckType chosenDeckType;
 
-    public LogInMessage(String senderPlayer) {
+    public LogInMessage(String senderPlayer, TowerColor chosenTowerColor, DeckType chosenDeckType) {
         super(MessageType.LOGIN, senderPlayer);
+        this.chosenTowerColor = chosenTowerColor;
+        this.chosenDeckType = chosenDeckType;
     }
 
+    public TowerColor getChosenTowerColor() {
+        return chosenTowerColor;
+    }
+
+    public DeckType getChosenDeckType() {
+        return chosenDeckType;
+    }
 }
