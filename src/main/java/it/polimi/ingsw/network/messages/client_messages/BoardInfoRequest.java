11f9d5bc5eca679160e5a29b5587a763c23af89c
maxperna <maxperna@users.noreply.github.com>
2022-05-30 16:54:06 +0200

    fix

--- src/main/java/it/polimi/ingsw/network/messages/client_messages/BoardInfoRequest.java
+++ src/main/java/it/polimi/ingsw/network/messages/client_messages/BoardInfoRequest.java
@@ -1,14 +1,12 @@
 package it.polimi.ingsw.network.messages.client_messages;
 
-import it.polimi.ingsw.model.Board;
 import it.polimi.ingsw.network.messages.Message;
 import it.polimi.ingsw.network.messages.MessageType;
 
-import java.util.UUID;
 
 public class BoardInfoRequest extends Message {
 
-    public BoardInfoRequest(String senderPlayer,String boardPlayer){
+    public BoardInfoRequest(String senderPlayer){
         super(MessageType.BOARD_INFO,senderPlayer);
     }
 
