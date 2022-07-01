5d4618dc778cc29c8b6a080088a830b4a3babf06
Lorenzo Migliari <lorenzo1.migliari@mail.polimi.it>
2022-07-01 21:31:34 +0200

    final?

--- src/main/java/it/polimi/ingsw/ClientApp.java
+++ src/main/java/it/polimi/ingsw/ClientApp.java
@@ -7,7 +7,7 @@ import javafx.application.Application;
 
 public class ClientApp {
     public static void main(String[] args) {
-        boolean Cli = true;
+        boolean Cli = false;
 
         for (String param : args) {
             if (param.equals("-c") || param.equals("--cli")) {
